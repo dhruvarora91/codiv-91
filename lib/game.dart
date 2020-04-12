3d52f836c5b94713805438651dcbade5360b85ff
vaibhavsahni009 <vaibhavsahni009@gmail.com>
2020-04-12 20:53:53 +0530

    bohot kaam bacha hae

--- lib/game.dart
+++ lib/game.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tindercard/flutter_tindercard.dart';
 import 'constants.dart';
 
-class GamePage extends StatelessWidget {
+int moneyVar = 20;
+
+enum dir { left, right }
+
+class GamePage extends StatefulWidget {
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   List<String> cardImages = [
     'images/characterimg1.png',
     'images/characterimg2.png',
@@ -15,91 +25,142 @@ class GamePage extends StatelessWidget {
     'images/characterimg4.png',
     'images/characterimg5.png',
   ];
+
   @override
   Widget build(BuildContext context) {
-    final int moneyVar = 20;
     final int personVar = 20;
     final int foodVar = 20;
     final int healthVar = 20;
+    CardController controller; //Use this to trigger swap.
+    dir direction;
+    AlignmentGeometry a = Alignment.topCenter;
+
+    String choice = "";
+
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: <Widget>[
             Expanded(
+                flex: 2,
                 child: Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  padding: const EdgeInsets.only(
+                    top: 30.0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Factors(
+                        icon: Icons.attach_money,
+                        value: moneyVar,
+                      ),
+                      Factors(
+                        icon: Icons.person_outline,
+                        value: personVar,
+                      ),
+                      Factors(
+                        icon: Icons.restaurant,
+                        value: foodVar,
+                      ),
+                      Factors(
+                        icon: Icons.local_hospital,
+                        value: healthVar,
+                      ),
+                    ],
+                  ),
+                )),
+            Expanded(
+              flex: 8,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(
-                        Icons.attach_money,
-                        size: 50.0,
-                        color: Colors.grey,
-                      ),
-                      Text(
-                        moneyVar.toString(),
-                        style: kNumberTextStyle,
-                      ),
-                    ],
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'question\nquestion\nquestion\nquestion\nquestion\n',
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                  Column(
-                    children: <Widget>[
-                      Icon(
-                        Icons.person_outline,
-                        size: 50.0,
-                        color: Colors.grey,
-                      ),
-                      Text(
-                        personVar.toString(),
-                        style: kNumberTextStyle,
-                      ),
-                    ],
+                  Expanded(
+                    flex: 6,
+                    child: TinderSwapCard(
+                        orientation: AmassOrientation.BOTTOM,
+                        totalNum: cardImages.length,
+                        stackNum: 3,
+                        swipeEdge: 4.0,
+                        maxWidth: MediaQuery.of(context).size.width * 1.0,
+                        maxHeight: MediaQuery.of(context).size.width * 1.4,
+                        minWidth: MediaQuery.of(context).size.width * 0.5,
+                        minHeight: MediaQuery.of(context).size.width * 0.7,
+                        cardBuilder: (context, index) => Card(
+                              child: Stack(
+                                children: <Widget>[
+                                  Image.asset('${cardImages[index]}'),
+                                  Container(
+                                      alignment: a,
+                                      child: Text(
+                                        'choice',
+                                        style: TextStyle(
+                                            color: Colors.pink,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 22.0),
+                                      )),
+                                ],
+                              ),
+                            ),
+                        cardController: controller = CardController(),
+                        swipeUpdateCallback:
+                            (DragUpdateDetails details, Alignment align) {
+                          /// Get swiping card's alignment
+                          setState(() {
+//                            if (align.x == 0) {
+//                              choice = '';
+//                            }
+                            if (align.x < 0) {
+                              //Card is LEFT swiping
+                              print('Going left');
+                              choice = 'choice left';
+                              a = Alignment.topRight;
+//
+//                              align = Alignment.topRight;
+                            } else if (align.x > 0) {
+                              //Card is RIGHT swiping
+                              print('Going right');
+
+                              choice = 'choice right';
+
+                              a = Alignment.topLeft;
+                            }
+                          });
+                        },
+                        swipeCompleteCallback:
+                            (CardSwipeOrientation orientation, int index) {
+                          /// Get orientation & index of swiped card!
+
+                          if (orientation == CardSwipeOrientation.LEFT) {
+                            setState(() {
+                              moneyVar++;
+                            });
+                            print('went left');
+                          } else if (orientation ==
+                              CardSwipeOrientation.RIGHT) {
+                            setState(() {
+                              moneyVar--;
+                            });
+                            print('went right');
+                          }
+                        }),
                   ),
-                  Column(
-                    children: <Widget>[
-                      Icon(
-                        Icons.restaurant,
-                        size: 50.0,
-                        color: Colors.grey,
-                      ),
-                      Text(
-                        foodVar.toString(),
-                        style: kNumberTextStyle,
-                      ),
-                    ],
+                  SizedBox(
+                    height: 15,
                   ),
-                  Column(
-                    children: <Widget>[
-                      Icon(
-                        Icons.local_hospital,
-                        size: 50.0,
-                        color: Colors.grey,
-                      ),
-                      Text(
-                        healthVar.toString(),
-                        style: kNumberTextStyle,
-                      ),
-                    ],
+                  Expanded(
+                    child: Text(
+                      'character name',
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ],
-              ),
-            )),
-            Expanded(
-              flex: 5,
-              child: TinderSwapCard(
-                orientation: AmassOrientation.BOTTOM,
-                totalNum: cardImages.length,
-                stackNum: 3,
-                swipeEdge: 4.0,
-                maxWidth: MediaQuery.of(context).size.width * 1.0,
-                maxHeight: MediaQuery.of(context).size.width * 1.4,
-                minWidth: MediaQuery.of(context).size.width * 0.5,
-                minHeight: MediaQuery.of(context).size.width * 0.7,
-                cardBuilder: (context, index) => Card(
-                  child: Image.asset('${cardImages[index]}'),
-                ),
               ),
             ),
             Expanded(
@@ -112,18 +173,25 @@ class GamePage extends StatelessWidget {
   }
 }
 
-//Row(
-//mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//children: <Widget>[
-//Icon(Icons.attach_money),
-//Icon(Icons.person_outline),
-//Icon(Icons.fastfood),
-//Icon(Icons.local_hospital),
-//],
-//),
+class Factors extends StatelessWidget {
+  Factors({@required this.icon, @required this.value});
+  final IconData icon;
+  int value;
 
-//
-//maxWidth: MediaQuery.of(context).size.width * 0.9,
-//maxHeight: MediaQuery.of(context).size.width * 0.9,
-//minWidth: MediaQuery.of(context).size.width * 0.8,
-//minHeight: MediaQuery.of(context).size.width * 0.8,
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 50.0,
+          color: Colors.grey,
+        ),
+        Text(
+          value.toString(),
+          style: kNumberTextStyle,
+        ),
+      ],
+    );
+  }
+}
